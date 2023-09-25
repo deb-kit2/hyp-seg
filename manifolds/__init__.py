0dfbca7833f94baf9444704b5def81737ce21bcb
Deb <nick.kittu.deb@gmail.com>
2023-09-25 23:46:21 +0530

    h2hgcn files added

--- manifolds/__init__.py
+++ manifolds/__init__.py
@@ -2,3 +2,5 @@ from .base import ManifoldParameter
 from .euclidean import Euclidean
 from .hyperboloid import Hyperboloid
 from .poincare import PoincareBall
+from .lorentz import LorentzManifold
+from .stiefel import StiefelManifold
